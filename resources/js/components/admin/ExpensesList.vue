45faacd16db7afabbdfa83a428d6e54d6363e1b0
saavedraphp <18977975+saavedraphp@users.noreply.github.com>
2026-03-08 18:24:16 -0500

    [CON-164] Add column asset code in list expenses

--- resources/js/components/admin/ExpensesList.vue
+++ resources/js/components/admin/ExpensesList.vue
@@ -28,6 +28,7 @@ const headers = ref([
     {title: 'Título', key: 'title', align: 'start', sortable: true},
     {title: 'Tipo de presupuesto', key: 'annual_budget.budget_type.name', sortable: true},
     {title: 'Activo', key: 'asset_type', sortable: true},
+    {title: 'ID Activo', key: 'asset_code', sortable: true},
     {title: 'Monto', key: 'amount', sortable: true},
     {title: 'Fecha', key: 'expense_date_format', sortable: true},
     {title: 'Acciones', key: 'actions', sortable: false, align: 'end'},
